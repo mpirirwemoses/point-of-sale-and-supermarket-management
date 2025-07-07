e928131cc93eb8f572fc83b1486e5c502deb4aa2
Moses Mpirirwe <mpirirwemoses2@gmail.com>
2025-07-07 16:08:06 +0300

    new big changes made

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,14 @@ import Employment from './assets/Elements/NewEmployment'
 import Expenses from './assets/Elements/NewExpense'
 import StockManagement from './assets/Elements/StockManagement'
 import Banner from './components/Banner'
-// import Cal from './components/Cal'
-// import FoodProfile from './components/Foodprof'
-import Header from './components/Header'
-import FoodProfile from './components/Foodprof'
-import KitchenDashboard from './components/KitchenDashboard'
-import HealthProvider from './assets/context/HealthContext'
-import React, { useState } from 'react';
-// import Food from './components/foodUseComponent'
+
 
 function App() {
   // const [view, setView] = useState('order');
 
   return (
     <>
-      {/* <HealthProvider> */}
-      <Header />
-      <Banner />
-      {/* 
-      <div className="flex justify-center gap-4 my-6">
-        <button
-          className={`px-6 py-2 rounded-lg font-bold transition text-lg shadow-md ${view === 'order' ? 'bg-pink-600 text-white' : 'bg-white text-pink-600 border border-pink-300 hover:bg-pink-100'}`}
-          onClick={() => setView('order')}
-        >
-          Order Food
-        </button>
-        <button
-          className={`px-6 py-2 rounded-lg font-bold transition text-lg shadow-md ${view === 'kitchen' ? 'bg-pink-600 text-white' : 'bg-white text-pink-600 border border-pink-300 hover:bg-pink-100'}`}
-          onClick={() => setView('kitchen')}
-        >
-          Kitchen Dashboard
-        </button>
-      </div>
-      {view === 'order' ? <FoodProfile /> : <KitchenDashboard />}
-      */}
-      {/* </HealthProvider> */}
-
+     
       <Inventory />
       <Expenses />
       <Employment />
